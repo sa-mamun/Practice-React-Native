697f5676238cf7d0f1f3177999d97c74b226681b
samir <samamun009@gmail.com>
2023-06-04 23:01:31 +0600

    Alert, Screen to screen navigation, SafeAreaView added

---
+++ GuessMyNumberApp/screens/GameScreen.js
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+
+function GameScreen() {
+    return (
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            <View>
+                <Text>Higher or Lower</Text>
+            </View>
+        </View>
+    );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    }
+});
