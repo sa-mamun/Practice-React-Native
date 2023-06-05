9b48a6a8993262f8a510379689cde87ab17396fa
samir <samamun009@gmail.com>
2023-06-05 22:52:04 +0600

    Title, Number Guess added

---
+++ GuessMyNumberApp/components/ui/Title.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text } from "react-native";
+
+import Colors from "../../constants/Colors";
+
+function Title({children}) {
+    return (
+        <Text style={styles.title}>{children}</Text>
+    );
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: 'white',
+        padding: 12
+    }
+});
--- GuessMyNumberApp/screens/GameScreen.js
+++ GuessMyNumberApp/screens/GameScreen.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    }
+    else {
+        return rndNum;
+    }
+}
+
+function GameScreen({ userNumber }) {
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
     return (
         <View style={styles.screen}>
-            <Text>Opponent's Guess</Text>
+            <Title>Opponent's Guess</Title>
+            <NumberContainer>{currentGuess}</NumberContainer>
             <View>
                 <Text>Higher or Lower</Text>
             </View>
